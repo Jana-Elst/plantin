43a9882482fe87fda1539251622d323ae0568d7e
Jana Elst <jana.elst@student.howest.be>
2025-01-21 12:02:54 +0100

    paintings responsive

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,8 @@ import '/src/css/style.css'
 import '/src/css/header.css'
 import '/src/css/navigation.css'
 import '/src/css/intro.css'
-import '/src/css/paintings.css'
 import '/src/css/gossip.css'
+import '/src/css/paintings.css'
 
 const $body = document.querySelector('.body');
 $body.classList.add("has-js");
