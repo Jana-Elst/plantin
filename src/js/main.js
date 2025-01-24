2cf1f46e1bbf9306ee562fef5ef8677e24b7efce
Jana Elst <jana.elst@student.howest.be>
2025-01-25 00:45:02 +0100

    swipers toegevoegd

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Swiper from 'swiper';
+
+import Swiper from 'swiper/bundle';
+
 import { hamburgerMenu } from './hamburger-menu'
 import {animations} from './animations'
 import { header } from './header'
@@ -8,8 +10,10 @@ import { auctionGame } from './auction-game'
 import { joinCompany } from './join-company-game'
 import { buttons } from './buttons'
 import {nav} from './nav'
+
 import '/src/css/reset.css'
 import '/src/css/style.css'
+import 'swiper/css/bundle';
 import '/src/css/header.css'
 import '/src/css/navigation.css'
 import '/src/css/intro.css'
@@ -31,11 +35,35 @@ $body.classList.add("has-js");
 
 gsap.registerPlugin(ScrollTrigger);
 
+const swiperPaintings = new Swiper('.swiper-paintings-smartphone', {
+    // modules: [Navigation, Pagination],
+    // Optional parameters
+    slidesPerView: "auto",
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+const swiperStories = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
 
 buttons();
 nav();
 hamburgerMenu();
-header();
-animations();
+//header();
+//animations();
 auctionGame();
 joinCompany();
